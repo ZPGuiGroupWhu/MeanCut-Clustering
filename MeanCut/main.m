34eafd467c17fabf4fdd81a0e521190d7e765c57
Dehua Peng <2410270794@qq.com>
2024-05-07 21:53:13 +0800

    Update main.m

--- MeanCut/main.m
+++ MeanCut/main.m
@@ -1,27 +1,17 @@
-%% Input the data
-data = textread('Synthetic Datasets/DS1.txt');
-[n,m] = size(data);
-X = data(:,1:2);
-ref = data(:,3);
+% Input the data
+clear;
+data = textread('Synthetic Datasets\DS1.txt');
+[~, m] = size(data);
+X = data(:, 1:m-1);
+ref = data(:, m);
 
-%% Perform MeanCut algorithm
-% DS1,DS2,DS3: knum = 20; percentile = 0; noiseT = 0;
-% DS4: knum = 20; percentile = 0; noiseT = 20;
-% DS5: knum = 20; percentile = 0; noiseT = 50;
-% DS6: knum = 20; percentile = 0.7; noiseT = 0;
-% DS7: knum = 20; percentile = 0.8; noiseT = 0;
-% DS8: knum = 20; percentile = 0.7; noiseT = 0;
-% DS9: knum = 20; percentile = 0.3; noiseT = 0;
-knum = 20;
-percentile = 0;
-noiseT = 0;
-[cluster] = ImprovedMeanCut(X, knum, percentile, noiseT);
+% Perform the MeanCut algorithm
+% DS6, DS7, DS8: k1 = 20; ratio=0.2, 0.7, 0.3;
+cluster = meancut(X);
 
-%% Visualize and evaluate the result
+% Evaluate the clustering accuracy
 addpath ClusterEvaluation
-plotcluster(X,cluster);
-[ ACC, NMI, ARI, Fscore, JI, RI] = ClustEval(ref, cluster);
+[ACC, NMI, ARI, Fscore, JI, RI] = ClustEval(ref, cluster);
 
-figure(2);
-bar([ACC, NMI, ARI, Fscore, JI, RI]);
-set(gca,'xticklabel',{'ACC','NMI','ARI','Fscore','JI','RI'})
+% Plot the clustering results
+plotcluster2(X, cluster);
